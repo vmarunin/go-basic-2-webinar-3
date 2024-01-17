a634bc8ee5347d79bb2987e4259e4d20d83c2497
Vladimir Marunin <v.marunin@vk.team>
2024-01-17 20:57:18 +0300

    closure

--- closure.go
+++ closure.go
@@ -1,10 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html"
+	"net/http"
+	"slices"
+	"sort"
+	"strings"
+)
 
 func ExampleClosure() {
 	// ExampleClosureSimple()
-	ExampleClosureLikeObject()
+	// ExampleClosureLikeObject()
+	// ExampleClosureHTTPServer()
+	ExampleClosureSort()
 }
 
 func ExampleClosureSimple() {
@@ -40,7 +49,7 @@ func ExampleClosureLikeObject() {
 	dec1(2)
 	fmt.Println(get1())
 
-	fmt.Println("One")
+	fmt.Println("Two")
 	inc2, dec2, get2 := CreateClosureLikeObject()
 	fmt.Println(get2())
 	inc2(30)
@@ -49,4 +58,49 @@ func ExampleClosureLikeObject() {
 	fmt.Println(get2())
 
 	fmt.Println("1 not changed", get1())
+}
+
+func ExampleClosureHTTPServer() {
+	// https://pkg.go.dev/net/http@go1.21.6#hdr-Servers
+	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+}
+
+func ExampleClosureSort() {
+	SortCaseIgnore()
+}
+
+func SortCaseIgnore() {
+	// Дан слайс строк, отсортировать его без учёта регистра
+	slice := []string{
+		"АНТОШКА", "АНтошка", "пойдём", "Копать", "картошку",
+		"антошкА", "антошКА", "пойдём", "копать", "карто-ошку",
+		"тили", "тили", "трали", "вали",
+	}
+
+	// Old way
+	unstable := make([]string, len(slice))
+	copy(unstable, slice)
+	// Unstable sort with Less
+	sort.Slice(unstable, func(i, j int) bool {
+		return strings.ToLower(unstable[i]) < strings.ToLower(unstable[j])
+	})
+	fmt.Println("Unstable sort", unstable)
+
+	// Go 1.21
+	stable := slices.Clone(slice)
+	// Stable sort with Compare
+	slices.SortStableFunc(stable, func(a, b string) int {
+		aLower, bLower := strings.ToLower(a), strings.ToLower(b)
+		if aLower == bLower {
+			return 0
+		}
+		if aLower < bLower {
+			return -1
+		}
+		return 1
+		//return cmp.Compare(strings.ToLower(a), strings.ToLower(b))
+	})
+	fmt.Println("Stable sort", stable)
 }
